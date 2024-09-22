f913798a7f07f165e636fc36c33f7dc245dbf49a
Lionel Foxcroft <lionelf329@gmail.com>
2024-09-22 01:52:31 -0400

    Remove HugeIcons

--- lib/widgets/medito_huge_icon.dart
+++ lib/widgets/medito_huge_icon.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hugeicons/hugeicons.dart';
 
 import '../constants/colors/color_constants.dart';
 
@@ -17,42 +16,6 @@ class MeditoHugeIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HugeIcon(
-      icon: _getIconData(icon),
-      color: color,
-      size: size,
-    );
-  }
-
-  IconData _getIconData(String iconString) {
-    switch (iconString) {
-      // custom ids:
-      case 'duohome':
-      case 'filledhome':
-        return HugeIcons.solidRoundedHome01;
-      case 'duoSearch':
-      case 'filledSearch':
-        return HugeIcons.solidRoundedSearch01;
-      case 'duoSettings':
-      case 'filledSettings':
-        return HugeIcons.solidRoundedSettings01;
-      // ids from the package:
-      case 'solidRoundedSun01':
-        return HugeIcons.solidRoundedSun01;
-      case 'solidRoundedDownloadSquare02':
-        return HugeIcons.solidRoundedDownloadSquare02;
-      case 'solidRoundedTime01':
-        return HugeIcons.solidRoundedTime01;
-      case 'solidRoundedSleeping':
-        return HugeIcons.solidRoundedSleeping;
-      case 'solidRoundedMedal06':
-        return HugeIcons.solidRoundedMedal06;
-      case 'solidRoundedHealtcare':
-        return HugeIcons.solidRoundedHealtcare;
-      case 'solidRoundedStar':
-        return HugeIcons.solidRoundedStar;
-      default:
-        return HugeIcons.solidRoundedQuestion;
-    }
+    return SizedBox(width: size, height: size);
   }
 }
